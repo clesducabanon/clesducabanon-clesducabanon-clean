75f7a3b47c6a9ef7806e0d945bfaad0cf29d02e7
les cles du cabanon <lesclesducabanon@mac-1.home>
2026-03-24 20:34:47 +0100

    restore graphic styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App.tsx';
-import './styles/theme.css';
-import './styles/fonts.css';
+import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
